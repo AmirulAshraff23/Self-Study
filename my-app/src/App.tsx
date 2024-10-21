7dc91733a907c1d639d3bd91b710db698aa574b6
AmirulAshraff23 <ashraffatwork92@gmail.com>
2024-10-21 14:45:08 +0800

    ss

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/style.css';
+import ToDo_Main from './components/ToDo_Main';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Geeks!</h1>
+      <h2>This is the 1st of the series</h2>
+      <p>Start typing here</p>
+      <ToDo_Main />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ToDo_Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ToDo_Main(){
+    return(
+        <div className='todo_container_main'>
+            <h2>To do list</h2>
+            <p>Let's get to work</p>
+            <input type="text" />
+            <input type="button" value="" />
+        </div>
+    );
+}
+
+export default ToDo_Main;
